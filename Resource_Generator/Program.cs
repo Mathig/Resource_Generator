5e7d696ff04887ef2bd30393c6e66574b0f7bf48
Mathig <mathignihilcek@gmail.com>
2018-08-21 12:40:58 -0700

    Add project files.

---
+++ Resource_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Resource_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
